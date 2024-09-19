0b177b3c69c32e291559c4ad9275c62843982660
91ac0m0 <zhngyln@bupt.edu.cn>
2024-09-19 14:32:01 +0800

    fix some crxjs problem

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
+import { crx, ManifestV3Export} from '@crxjs/vite-plugin'
+import man from './manifest.json' assert {type: 'json'}
+
+const manifest = man as ManifestV3Export;
 
 export default defineConfig({
   plugins: [
     react(),
     crx({ manifest }),
   ],
+  // see issue: https://github.com/crxjs/chrome-extension-tools/issues/746
+  server: {
+    strictPort: true,
+    port: 5173,
+    hmr: {
+      clientPort: 5173
+    }
+  }
 })
